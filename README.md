c3960a2380f2fd8934c62896ceae084af1d6f42a
David Zhang <davidzha@xilinx.com>
2021-03-30 14:02:59 -0700

    add missing files

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # XRT_RPU
+
+## System Requirements
+    - Build RPU platform and applications, linux with Xilinx Vitis software is recommended.
+    - Run RPU applications, a MPSOC based platform like vck5000, zcu102 are recommended.
+      Note: do not support hw emulation, like qemu.
+
+## Build Instructions
+    To build entire platform and applications:
+      cd build; ./build.sh
+
+    To build entire application:
+      cd build: ./build_rmgmt.sh
+
+    To build
+
+
+## Use Cases
+
+    1. Install customized shell and XRT
+    2. Flash shell
+    3. Program xclbin
+      
--- build/buildbsp.tcl
+++ build/buildbsp.tcl
@@ -2,7 +2,7 @@ puts "set workspace"
 setws . 
 
 puts "create platform as project_vck"
-platform create -out platform_vck -name "platform_vck" -hw ../xsa/gen3x16.xsa -proc psv_cortexr5_0 -os freertos10_xilinx
+platform create -out rmgmt_platform -name "rmgmt_platform" -hw ../xsa/gen3x16.xsa -proc psv_cortexr5_0 -os freertos10_xilinx
 
 #active platform
 #platform active platform_vck
@@ -16,14 +16,14 @@ bsp setlib xilmailbox
 bsp getlibs
 
 #repo add platform for apps
-repo -add-platform platform_vck
+repo -add-platform rmgmt_platform
 puts "repo list platforms"
 repo -platforms
 
 platform generate
 
 puts "create app"
-app create -name rmgmt -platform platform_vck -domain freertos10_xilinx_domain -template "Empty Application"
+app create -name rmgmt -platform rmgmt_platform -domain freertos10_xilinx_domain -template "Empty Application"
 
 puts "build app"
 app build -name rmgmt 
