f8aece260bea7a120affb8c278bc6cd2caa5d5c7
David Zhang <davidzha@xilinx.com>
2022-01-05 11:37:04 -0800

    adjust bsp_create

--- build/create_bsp.tcl
+++ build/create_bsp.tcl
@@ -5,11 +5,11 @@ puts "create platform as vmr_platform"
 platform create -out vmr_platform -name "vmr_platform" -hw xsa/gen3x16.xsa -proc psv_cortexr5_0 -os freertos10_xilinx
 
 puts "redirect output to uart lite between rpu and pcie mgmt"
-#bsp config stdin blp_blp_logic_axi_uart_rpu
-#bsp config stdout blp_blp_logic_axi_uart_rpu
+bsp config stdin blp_blp_logic_axi_uart_rpu
+bsp config stdout blp_blp_logic_axi_uart_rpu
 #this is for jtag
-bsp config stdin blp_cips_pspmc_0_psv_sbsauart_1
-bsp config stdout blp_cips_pspmc_0_psv_sbsauart_1
+#bsp config stdin blp_cips_pspmc_0_psv_sbsauart_1
+#bsp config stdout blp_cips_pspmc_0_psv_sbsauart_1
 
 puts "customize bsp libs"
 bsp setlib xilfpga
