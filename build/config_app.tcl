272c6055f948a48345cd762fda6dab8df517a138
David Zhang <50243230+xdavidz@users.noreply.github.com>
2022-03-07 10:46:44 -0800

    enable -Werror warning as error for VMR source code (#74)

--- build/config_app.tcl
+++ build/config_app.tcl
@@ -1,3 +1,6 @@
 setws .
-puts "config app"
+puts "=== config app"
+puts "=== add include"
 app config -name vmr -add include-path ../src/include
+puts "=== set warnings as errors"
+app config -name vmr -add compiler-misc -Werror
