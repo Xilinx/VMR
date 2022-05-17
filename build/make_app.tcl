772baac8c03658b3e150720ae279a67705885ef6
David Zhang <davidzha@xilinx.com>
2022-05-17 14:18:39 -0700

    SCL-657 Legal Sweep: Versal Management Runtime (VMR) for Versal Family

--- build/make_app.tcl
+++ build/make_app.tcl
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2018-2022 Xilinx, Inc. All rights reserved.
+# Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
 setws .
 puts "build app"
 app build -name vmr 
