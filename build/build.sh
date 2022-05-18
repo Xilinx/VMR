57c85c72b9e72579e48d0c564dce02a03be3c3a0
David Zhang <50243230+xdavidz@users.noreply.github.com>
2022-05-18 13:24:28 -0700

    SCL-657 Legal Sweep: Versal Management Runtime (VMR) for Versal Family (#144)
    * SCL-657 Legal Sweep: Versal Management Runtime (VMR) for Versal Family
    
    * remove binary file for license issue
    
    * remove XRT info
    
    * changed xclbin to GPL2

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2018-2022 Xilinx, Inc. All rights reserved.
+# Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
 TOOL_VERSION="2022.1"
 DEFAULT_VITIS="/proj/xbuilds/${TOOL_VERSION}_daily_latest/installs/lin64/Vitis/HEAD/settings64.sh"
 STDOUT_JTAG=0
