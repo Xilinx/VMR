90641e33d0638f6bbcfccf7a57093e185b7898c7
Kushagra <36882741+kbhardwa-xilinx@users.noreply.github.com>
2022-06-06 17:46:19 -0600

    Adding files for pipeline and TA change, which is tested (#150)
    * Adding docker files
    
    * Update build.json
    
    Co-authored-by: kbhardwa <kbhardwa@xilinx.com>

---
+++ docker/vmr_setup_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Setup TA
+cd ../build && TA=$(jq -r '.CONF_BUILD_TA' build.json)
+
+#Install XRT and APU packages
+sudo dpkg  -i /proj/xbuilds/${TA}/xbb/xrt/packages/xrt_*_20.04-amd64-xrt.deb
+sudo dpkg  -i /proj/xbuilds/${TA}/xbb/packages/internal_platforms/vck5000/gen4x8_xdma/*-dev/xilinx-vck5000-gen4x8-xdma-*_all.deb
+sudo dpkg  -i /proj/xbuilds/${TA}/xbb/packages/internal_platforms/vck5000/gen4x8_xdma/base/xilinx-vck5000-gen4x8-xdma-base_*_all.deb
+sudo dpkg -i /proj/xbuilds/${TA}/internal_platforms/sw/versal/apu_packages/versal/xrt-apu_*_all.deb
+
+#Source settings.sh
+source /proj/xbuilds/${TA}/installs/lin64/Vitis/HEAD/settings64.sh
+
+#Run VMR build
+./build.sh
+
+#Copy VMR.elf file to NFS location
+mkdir -p /proj/xbuilds/VMR-ELF/${RELEASE}/${BUILD_NUMBER} && cp *.elf /proj/xbuilds/VMR-ELF/${RELEASE}/${BUILD_NUMBER}
