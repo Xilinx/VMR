3332bcdc6c541e6e8ca5cc7167a7d5719012ffad
David Zhang <davidzha@xilinx.com>
2022-06-17 14:20:06 -0700

    VITIS-5847 Resilient VMR - pipeline should switch to qdma shell add stdin/stdout in vmr_endport stdin/stdout endpoints are correct in xparameters.h now

--- docker/vmr_setup_build.sh
+++ docker/vmr_setup_build.sh
@@ -5,11 +5,11 @@ cd ../build && TA=$(jq -r '.CONF_BUILD_TA' build.json)
 
 #Install XRT and APU packages
 sudo dpkg  -i /proj/xbuilds/${TA}/xbb/xrt/packages/xrt_*_20.04-amd64-xrt.deb
-sudo dpkg  -i /proj/xbuilds/${TA}/xbb/packages/internal_platforms/vck5000/gen4x8_xdma/*-dev/xilinx-vck5000-gen4x8-xdma-*_all.deb
-sudo dpkg  -i /proj/xbuilds/${TA}/xbb/packages/internal_platforms/vck5000/gen4x8_xdma/base/xilinx-vck5000-gen4x8-xdma-base_*_all.deb
-sudo dpkg -i /proj/xbuilds/${TA}/internal_platforms/sw/versal/apu_packages/versal/xrt-apu_*_all.deb
-sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/1-202210-1-dev/xilinx-vck5000-*_all.deb
+sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/packages/internal_platforms/vck5000/gen4x8_xdma/*-dev/xilinx-vck5000-*_all.deb
+sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/packages/internal_platforms/vck5000/gen4x8_xdma/base/xilinx-vck5000-*_all.deb
+sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/*-dev/xilinx-vck5000-*_all.deb
 sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/base/xilinx-vck5000-*_all.deb
+sudo dpkg -i /proj/xbuilds/${TA}/internal_platforms/sw/versal/apu_packages/versal/xrt-apu_*_all.deb
 
 
 #Source settings.sh
