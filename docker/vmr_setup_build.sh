7a3714b7e2c0ca48558450d3e8907cb48c93fa1e
David Zhang <50243230+xdavidz@users.noreply.github.com>
2022-06-30 14:15:57 -0700

    just remove xdma pkgs (#173)

--- docker/vmr_setup_build.sh
+++ docker/vmr_setup_build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Setup TA
-cd ../build && TA=$(jq -r '.CONF_BUILD_TA' build.json)
+#cd ../build && TA=$(jq -r '.CONF_BUILD_TA' build.json)
+cd ../build
+TA=2022.2_daily_latest
 
 #Install XRT and APU packages
 sudo dpkg  -i /proj/xbuilds/${TA}/xbb/xrt/packages/xrt_*_20.04-amd64-xrt.deb
-sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/packages/internal_platforms/vck5000/gen4x8_xdma/*-dev/xilinx-vck5000-*_all.deb
-sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/packages/internal_platforms/vck5000/gen4x8_xdma/base/xilinx-vck5000-*_all.deb
-sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/*-dev/xilinx-vck5000-*_all.deb
-sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/base/xilinx-vck5000-*_all.deb
+sudo dpkg -i /proj/xbuilds/${TA}/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/*-dev/xilinx-vck5000-*_all.deb
+sudo dpkg -i /proj/xbuilds/${TA}/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/base/xilinx-vck5000-*_all.deb
 sudo dpkg -i /proj/xbuilds/${TA}/internal_platforms/sw/versal/apu_packages/versal/xrt-apu_*_all.deb
 
 
