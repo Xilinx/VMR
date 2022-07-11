a916cc3188fb48ef30f84398658964c38994d0ec
David Zhang <50243230+xdavidz@users.noreply.github.com>
2022-07-11 15:31:24 -0700

    VITIS-5847 Resilient VMR - add stack analyzer for VMR code (#172)
    * VITIS-5847 Resilient VMR - pipeline get ride of xdma shell
    
    * change TA to 0628_1
    
    * add stack analyzing tool stack_usage.sh
    
    * fix typo

--- build/config_app.tcl
+++ build/config_app.tcl
@@ -7,3 +7,7 @@ puts "=== add include"
 app config -name vmr_app -add include-path ../src/include
 puts "=== set warnings as errors"
 app config -name vmr_app -add compiler-misc -Werror
+puts "=== analyze stack usage per function"
+app config -name vmr_app -add compiler-misc -fstack-usage
+puts "=== set warnings if stack is above 4k"
+app config -name vmr_app -add compiler-misc -Wstack-usage=4096
