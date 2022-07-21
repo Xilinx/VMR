6e221930c25bf0301bf00167e8fd9a424b1b280b
Bhargava Sreekantappa Gayathri <bhargava.sreekantappa-gayathri@xilinx.com>
2022-07-21 11:44:05 -0700

    Update LM75 driver code
    This patch updates the temperature read api as per the data sheet to get
    the correct readings.
    
    Signed-off-by: Bhargava Sreekantappa Gayathri <bhargava.sreekantappa-gayathri@xilinx.com>

--- vmr/src/vmc/sensors/src/lm75.c
+++ vmr/src/vmc/sensors/src/lm75.c
@@ -9,7 +9,7 @@
 u8 LM75_ReadTemperature(u8 i2c_num, u8 slaveAddr, s32 *temperatureValue)
 {
     u8 status = 0;
-    s32 TempHexVal = 0;
+    u16 TempHexVal = 0;
     u8 i2c_read_buff[2] = {0};
     u8 i2c_read_len = 2;
     u8 i2c_write_buff[2];
@@ -18,32 +18,25 @@ u8 LM75_ReadTemperature(u8 i2c_num, u8 slaveAddr, s32 *temperatureValue)
     
     status = i2c_send_rs_recv(i2c_num, slaveAddr, &i2c_write_buff[0], 1, &i2c_read_buff[0], i2c_read_len);
     if(0 == status)
-    {
-	/* only 11 MSB should be used , 5 LSB bits are zero and ignored  */
-	TempHexVal = i2c_read_buff[1] | (i2c_read_buff[0] << 8);
-	TempHexVal = TempHexVal >> 5;
+    	{
+		TempHexVal = i2c_read_buff[1] | (i2c_read_buff[0] << 8);
+		TempHexVal = TempHexVal >> 7;
 
-	/* The 11th bit is the sign bit ,
-  	 *
-  	 * For +ve temperature:
-  	 * TempHexVal * 0.125 = Actual Temperature
-  	 *
-  	 * For -ve Temperature:
-  	 * -(two's compliment of TempHexVal) * 0.125 = Actual Temperature
-  	 */
+		const int negative = (TempHexVal & (1 << 8)) != 0;
+		TempHexVal &= (0xFF);
+		s16 nativeInt = 0;
 
-	if(TempHexVal < 0x3FF) 
-	{
-	    /* Positive Temperature */
-	    *temperatureValue = (int)TempHexVal * 0.125;
-	}
-	else 
-	{
-	    /* Negative Temperature */
-	    TempHexVal = TempHexVal & 0x7FF;
-	    *temperatureValue = (int)(0x800 - TempHexVal ) * 0.125 * (-1);
-	}
-    }
+		if (negative)
+		{
+  			nativeInt = TempHexVal | ~((1 << 8) - 1);
+			*temperatureValue = (s16)(nativeInt/2);
+		}
+		else
+		{
+			nativeInt = TempHexVal;
+			*temperatureValue = (s16)(nativeInt/2);
+    	}
+		}
 
     return status;
 }
