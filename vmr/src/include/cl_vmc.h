c54c836bd59f6853bbe7f4af195f4f038a012939
Sandeep Kumar Thakur <73092092+sandeept8@users.noreply.github.com>
2022-08-17 08:51:33 +0530

    VMC : Enable Clock throttling XRT command (#191)
    New XRT command can Enable or Disable Clock throttling,
    and set and override throttling limits on top of Default limits
    for internal testing.
    
    Depends on: Xilinx/XRT#6876
    
    Signed-off-by: Sandeep Thakur<sandeep.thakur@amd.com>
    
    Signed-off-by: Sandeep Thakur<sandeep.thakur@amd.com>

--- vmr/src/include/cl_vmc.h
+++ vmr/src/include/cl_vmc.h
@@ -6,8 +6,30 @@
 #ifndef COMMON_VMC_H
 #define COMMON_VMC_H
 
+#include "xil_types.h"
+
 struct cl_msg;
 struct fpt_sc_version;
+
+typedef struct __attribute__((packed)) {
+	u8 shutdown_limit_temp;
+	u16 shutdown_limit_pwr;
+	u8 throttle_limit_temp;
+	u16 throttle_limit_pwr;
+}temp_power_limits_t;
+
+typedef struct  __attribute__((packed)) {
+	u8 is_clk_scaling_supported;
+	u8 clk_scaling_mode;
+	u8 clk_scaling_enable;
+	temp_power_limits_t limits;
+
+	/* New override values recvd, push into Algo */
+	u8 temp_throttling_enabled;
+	u8 power_throttling_enabled;
+	u8 limits_update_req;
+}clk_throttling_params_t;
+
 
 int cl_vmc_sensor(struct cl_msg *msg);
 int cl_vmc_is_ready(void);
@@ -26,4 +48,6 @@ int cl_vmc_scfw_program_progress(void);
 int cl_vmc_sysmon_init(void);
 int cl_vmc_sysmon_is_ready(void);
 
+int cl_vmc_clk_scaling(struct cl_msg *msg);
+void cl_vmc_get_clk_throttling_params(clk_throttling_params_t *params);
 #endif
