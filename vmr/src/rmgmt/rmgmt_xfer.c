595275abf47966e05f3377f5a7aeb0ba40744448
David Zhang <davidzha@xilinx.com>
2022-11-01 21:56:27 -0700

    CR-1143441 reconfig clock after xclbin download and before pr free
    Signed-off-by: David Zhang <davidzha@xilinx.com>

--- vmr/src/rmgmt/rmgmt_xfer.c
+++ vmr/src/rmgmt/rmgmt_xfer.c
@@ -16,6 +16,10 @@
 #include "cl_flash.h"
 #include "cl_rmgmt.h"
 
+#define CLK_TYPE_DATA   0
+#define CLK_TYPE_KERNEL 1
+#define CLK_TYPE_SYSTEM 2
+#define CLK_TYPE_MAX    4
 
 static inline u32 wait_for_status(struct rmgmt_handler *rh, u8 status)
 {
@@ -255,10 +259,91 @@ static int disable_kernel_clock()
 	return sensor_task_request(&msg);
 }
 
-/*TODO: reconfig clock after new xclbin is programmed */
-static int reconfig_clock()
+static int freq_scaling_internal(unsigned short *freqs, int num_freqs)
+{	
+	cl_msg_t msg = { 0 };
+
+	/* This is an internal clock scaling command */
+	msg.clock_payload.ocl_req_type = CL_CLOCK_SCALE_INTERNAL;
+	msg.clock_payload.ocl_req_num = num_freqs;
+
+	for (int i = 0; i < num_freqs; i++)
+		msg.clock_payload.ocl_req_freq[i] = freqs[i];
+
+	return cl_rmgmt_clock(&msg);	
+}
+
+static int reconfig_clock(const char *clock, int clock_size)
 {
-	return 0;
+	struct clock_freq_topology *topo;
+	struct clock_freq *freq = NULL;
+	int data_clk_count = 0;
+	int kernel_clk_count = 0;
+	int system_clk_count = 0;
+	int clock_type_count = 0;
+	int i = 0;
+	unsigned short target_freqs[CLK_TYPE_MAX] = { 0 };
+
+	if (clock == NULL || clock_size == 0) {
+		VMR_WARN("skip config");
+		return 0;
+	}
+
+	topo = (struct clock_freq_topology *)clock;
+
+	if (topo->m_count > CLK_TYPE_MAX) {
+		VMR_ERR("More than %d clocks found in colock topology", CLK_TYPE_MAX);
+		return -EINVAL;
+	}
+
+        for (i = 0; i < topo->m_count; i++) {
+                freq = &(topo->m_clock_freq[i]);
+                if (freq->m_type == CT_DATA)
+                        data_clk_count++;
+                if (freq->m_type == CT_KERNEL)
+                        kernel_clk_count++;
+                if (freq->m_type == CT_SYSTEM)
+                        system_clk_count++;
+        }
+
+        if (data_clk_count != 1) {
+                VMR_ERR("Data clock not found in clock topology");
+                return -EINVAL;
+        }
+        if (kernel_clk_count != 1) {
+                VMR_ERR("Kernel clock not found in clock topology");
+                return -EINVAL;
+        }
+        if (system_clk_count > 2) {
+                VMR_ERR("More than 2 system clocks found in clock topology");
+                return -EINVAL;
+        }
+
+	/* system clock start index */
+        clock_type_count = CLK_TYPE_SYSTEM;
+        for (i = 0; i < topo->m_count; i++) {
+                freq = &(topo->m_clock_freq[i]);
+		switch (freq->m_type) {
+		case CT_DATA:
+                        target_freqs[CLK_TYPE_DATA] = freq->m_freq_Mhz;
+			break;
+		case CT_KERNEL:
+                        target_freqs[CLK_TYPE_KERNEL] = freq->m_freq_Mhz;
+			break;
+		case CT_SYSTEM:
+                        target_freqs[clock_type_count++] = freq->m_freq_Mhz;
+			break;
+		default:
+			VMR_ERR("Unknown type: %d", freq->m_type);
+			return -EINVAL;
+		}
+        }
+
+        VMR_LOG("set %lu freq, data: %d, kernel: %d, sys: %d, sys1: %d",
+            ARRAY_SIZE(target_freqs), target_freqs[0], target_freqs[1],
+            target_freqs[2], target_freqs[3]);
+	
+	return freq_scaling_internal(target_freqs, ARRAY_SIZE(target_freqs));
 }
 
 static int enable_kernel_clock()
@@ -269,7 +354,7 @@ static int enable_kernel_clock()
 	return sensor_task_request(&msg);
 }
 
-int fpga_pdi_download(UINTPTR data, UINTPTR size, int has_pl)
+int fpga_pdi_download(UINTPTR data, UINTPTR size, const char *clock, int clock_size, int has_pl)
 {
 	int ret;
 	XFpga XFpgaInstance = { 0U };
@@ -294,20 +379,21 @@ int fpga_pdi_download(UINTPTR data, UINTPTR size, int has_pl)
 	ret = XFpga_BitStream_Load(&XFpgaInstance, data, KeyAddr, size, PDI_LOAD);
 
 	if (has_pl) {
-		reconfig_clock();
+		reconfig_clock(clock, clock_size);
 		ucs_start();
 		MDELAY(10);
 		axigate_free();
 		enable_kernel_clock();
 	}
 
-	VMR_LOG("ret: %d \r\n", ret);
+	VMR_LOG("ret: %d", ret);
 	return ret;
 }
 
-static inline int pdi_download(UINTPTR data, UINTPTR size, int has_pl)
+static inline int pdi_download(UINTPTR data, UINTPTR size, const char *clock,
+	int clock_size, int has_pl)
 {
-	return fpga_pdi_download(data, size, has_pl);
+	return fpga_pdi_download(data, size, clock, clock_size, has_pl);
 }
 
 static int rmgmt_fpga_download(struct rmgmt_handler *rh, u32 len)
@@ -316,16 +402,35 @@ static int rmgmt_fpga_download(struct rmgmt_handler *rh, u32 len)
 	struct axlf *axlf = (struct axlf *)rh->rh_data;
 	uint64_t offset = 0;
 	uint64_t size = 0;
+	char *partial_pdi = NULL;
+	char *pdi = NULL;
+	char *xclbin_topo = NULL;
+	u32 partial_pdi_size = 0;
+	u32 pdi_size = 0;
+	u32 xclbin_topo_size = 0;
 
 	/* Sync data from cache to memory */
 	Xil_DCacheFlush();
 
+	ret = rmgmt_xclbin_section_info(axlf, CLOCK_FREQ_TOPOLOGY, &offset, &size);
+	if (ret || size == 0) {
+		VMR_LOG("no CLOCK TOPOLOGY from xclbin: %d", ret);
+	} else {
+		xclbin_topo = (char *)axlf + offset;
+		xclbin_topo_size = size;
+	}
+
 	ret = rmgmt_xclbin_section_info(axlf, BITSTREAM_PARTIAL_PDI, &offset, &size);
 	if (ret || size == 0) {
-		VMR_LOG("no PARTIAL PDI from xclbin: %d", ret);
+		VMR_WARN("no PARTIAL PDI from xclbin: %d", ret);
 	} else {
-		ret = pdi_download((UINTPTR)((const char *)axlf + offset),
-			(UINTPTR)size, 1);
+		partial_pdi = (char *)axlf + offset;
+		partial_pdi_size = size;
+		VMR_LOG("PARTIAL PDI from xclbin size:%d", partial_pdi_size);
+
+		ret = pdi_download((UINTPTR)partial_pdi, (UINTPTR)partial_pdi_size,
+			xclbin_topo, xclbin_topo_size, 1);
+		
 		if (ret)
 			goto done;
 	}
@@ -334,18 +439,23 @@ static int rmgmt_fpga_download(struct rmgmt_handler *rh, u32 len)
 	if (ret || size == 0) {
 		VMR_LOG("no PDI from xclbin: %d", ret);
 	} else {
-		ret = pdi_download((UINTPTR)((const char *)axlf + offset),
-			(UINTPTR)size, 1);
+		pdi = (char *)axlf + offset;
+		pdi_size = size;
+		VMR_LOG("PDI from xclbin size:%d", pdi_size);
+
+		ret = pdi_download((UINTPTR)pdi, (UINTPTR)pdi_size,
+			xclbin_topo, xclbin_topo_size, 1);
+
 		if (ret)
 			goto done;
 	}
 
 	ret = rmgmt_apu_download_xclbin(rh);
 	if (ret == -ENODEV) {
-		VMR_LOG("skip apu download xclbin ret: %d", ret);
+		VMR_WARN("skip apu download xclbin ret: %d", ret);
 		ret = 0;
 	} else if (ret) {
-		VMR_LOG("failed apu download xclbin ret: %d", ret);
+		VMR_ERR("failed apu download xclbin ret: %d", ret);
 	}
 
 done:
@@ -380,7 +490,7 @@ int rmgmt_load_apu(struct rmgmt_handler *rh)
 	/* Sync data from cache to memory */
 	Xil_DCacheFlush();
 
-	ret = pdi_download((UINTPTR)rh->rh_data, (UINTPTR)size, 0);
+	ret = pdi_download((UINTPTR)rh->rh_data, (UINTPTR)size, NULL, 0, 0);
 
 	return ret;
 }
@@ -448,9 +558,10 @@ static int rmgmt_ospi_apu_download(struct rmgmt_handler *rh, u32 len)
 	/*
 	 * when loading an APU or any other PDI type which does not change the ULP.
 	 * set ulp_changed to false.
+	 * APU PDI doesn't have clock topology, thus set to NULL.
 	 */
-	ret = pdi_download((UINTPTR)((const char *)axlf + offset),
-		(UINTPTR)size, 0);
+	ret = pdi_download((UINTPTR)((const char *)axlf + offset), (UINTPTR)size,
+		NULL, 0, 0);
 
 	VMR_LOG("FPGA load pdi ret: %d", ret);
 	return ret;
