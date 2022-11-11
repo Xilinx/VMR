3c51c57c78994b7150a793aee97bc12b01b81676
David Zhang <davidzha@xilinx.com>
2022-11-11 11:56:32 -0800

    add dev pkg

--- docker/vmr_setup_build.sh
+++ docker/vmr_setup_build.sh
@@ -10,8 +10,9 @@ sudo dpkg  -i /proj/xbuilds/${TA}/xbb/xrt/packages/xrt_*_20.04-amd64-xrt.deb
 sudo dpkg -i /proj/xbuilds/${TA}/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/*-dev/xilinx-vck5000-*_all.deb
 #Use previous stable base pkg
 #sudo dpkg -i /proj/xbuilds/${TA}/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/base/xilinx-vck5000-*_all.deb
+sudo dpkg -i /proj/xbuilds/2022.2_1102_1/xbb/packages/internal_platforms/vck5000/gen4x8_qdma/2-202220-1-dev/xilinx-vck5000-gen4x8-qdma-2-202220-1-dev_1-20221024_all.deb
 sudo dpkg -i /proj/sdxbf/xrt-test/2022.2/xilinx_vck5000_gen4x8_qdma_2_202220_1/last_stable/dsabin/xilinx-vck5000-*_all.deb
-sudo dpkg -i /proj/xbuilds/${TA}/internal_platforms/sw/versal/apu_packages/versal/xrt-apu_*_all.deb
+sudo dpkg -i /proj/xbuilds/2022.2_daily_latest/xbb/xrt/packages/apu_packages/xrt-apu-vck5000*all.deb
 
 #Run VMR build
 ./build.sh
