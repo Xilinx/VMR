18923b9c4ef256d7de0d28a3928a1298f6748b44
David Zhang <davidzha@xilinx.com>
2023-02-13 08:52:21 -0800

    CR-1153211 set RTOS task depth based on peak memory usage
    Signed-off-by: David Zhang <davidzha@xilinx.com>

--- vmr/src/common/cl_main.c
+++ vmr/src/common/cl_main.c
@@ -23,8 +23,14 @@
 
 #define XGQ_XQUEUE_LENGTH	8
 #define XGQ_XQUEUE_WAIT_MS	10
-/* Depth "number of words" of the stack. depth (64k) * sizeof(word) = total size (256k) */
-#define TASK_STACK_DEPTH 	0x10000
+/* 
+ * use check_usage.sh to check peak usage in theory.
+ * use vmr_mem_status to check peak usage in certain load.
+ * the current peak usage is < 2k, seting thread to 16k heap size.
+ *  The thread Depth "number of words" of the stack.
+ *  depth (4k) * sizeof(word) = total size (16k)
+ */
+#define TASK_STACK_DEPTH 	0x1000
 
 /*
  * VMR (Versal Management Runtime) design diagram.
