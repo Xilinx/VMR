9be1f7a17926aacfdb12059f07dc51febebc2edc
Thirukumaran K <thirukum@xhdthirukum40x.xlnx.xilinx.com>
2023-02-16 17:17:00 +0530

    Migrated from Makefile to CMake build method for Unittest
    - Removed Makefile and Developed test build and test coverage build method using CMake
    
    Signed-off-by: Thirukumaran K <thirukumaran.k@amd.com>

--- vmr/src/vmc/ut/test/vmc/sensor/se98a/unittest.mk
+++
@@ -1,9 +0,0 @@
-SRC += $(VMR_MAIN)/vmr/src/vmc/sensors/src/se98a.c \
-       $(ROOT_DIR)/mocks/common/utm_cl_i2c.c \
-       $(ROOT_DIR)/test/vmc/sensor/se98a/utt_se98a.c 
-
-#Source code for which code coverage report will be generated
-COV_SRC = $(VMR_MAIN)/vmr/src/vmc/sensors/src/se98a.c
-
-MOCKS += i2c_send_rs_recv
-
