5d428b5ea3d3eba91dab09ac67cdbca100770a33
apaul <apaul@xilinx.com>
2023-05-11 16:41:34 +0100

    ALVEO-12951: Minor change

--- vmr/src/vmc/sensors/src/lm75.c
+++ vmr/src/vmc/sensors/src/lm75.c
@@ -17,7 +17,7 @@ u8 LM75_ReadTemperature( u8 i2c_num, u8 slaveAddr, s16 *temperatureValue )
     i2c_write_buff[0] = 0x00;
     
     status = i2c_send_rs_recv( i2c_num, slaveAddr, &i2c_write_buff[0], 1, &i2c_read_buff[0], i2c_read_len );
-    if( 0 == status  )
+    if( 0 == status )
     {
         /* Arrange the bytes in  the correct order */
         TempHexVal = i2c_read_buff[1] | ( i2c_read_buff[0] << 8 );
@@ -25,14 +25,14 @@ u8 LM75_ReadTemperature( u8 i2c_num, u8 slaveAddr, s16 *temperatureValue )
         /* Lowest 7 bits are undefined so shift right */
         TempHexVal = TempHexVal >> 7;
 
-        /* Bit 8 (MSB ) determines if the number is positive or negative */
+        /* Bit 8 (MSB) determines if the number is positive or negative */
         const int negative = ( TempHexVal & ( 1 << 8 ) ) != 0;  
         TempHexVal &= ( 0xFF );
         s16 nativeInt = 0;
 
         /* If the value was negative we need to correct the MSB to reflect that 
         and finally divide by 2 to change the temperature to a whole number */
-        if( negative  )
+        if( negative )
         {
             nativeInt = TempHexVal | ~( ( 1 << 8 ) - 1 );
             *temperatureValue = ( s16 )( nativeInt/2 );
